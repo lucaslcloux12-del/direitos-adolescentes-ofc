5ca0e712a8b6f3e14b7142fd99c21afaf95ae11f
lucaslcloux12-del <lucaslcloux12@gmail.com>
2026-04-10 01:56:17 -0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA3iDOgb5__CH93gdOmRBmp9SJfg5t8bMA",
+  authDomain: "brasil-org-ofc.firebaseapp.com",
+  projectId: "brasil-org-ofc",
+  storageBucket: "brasil-org-ofc.firebasestorage.app",
+  messagingSenderId: "6647327645",
+  appId: "1:6647327645:web:b1cd3696dc94fcb3395d74"
 };
 
 const app = initializeApp(firebaseConfig);
